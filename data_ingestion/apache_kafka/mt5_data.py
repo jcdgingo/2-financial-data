3a2e7988d2bee2c388a9367793774d25e25b1cfe
Jess <jcdgingo@gmail.com>
2023-10-31 18:45:31 -0400

    created scripts for metatrader API

--- data_ingestion/apache_kafka/mt5_data.py
+++ data_ingestion/apache_kafka/mt5_data.py
@@ -20,9 +20,9 @@ if not mt5.initialize(login=51439076, server="ICMarketsSC-Demo",password="vwYYua
 # set time zone to UTC
 timezone = pytz.timezone("Etc/UTC")
 # create 'datetime' object in UTC time zone to avoid the implementation of a local time zone offset
-utc_from = datetime(2020, 1, 10, tzinfo=timezone)
+utc_from = datetime(2023, 10, 25, tzinfo=timezone)
 # get 10 EURUSD H4 bars starting from 01.10.2020 in UTC time zone
-rates = mt5.copy_rates_from("EURUSD", mt5.TIMEFRAME_H4, utc_from, 10)
+rates = mt5.copy_rates_from("EURUSD", mt5.TIMEFRAME_M1, utc_from, 100)
  
 # shut down connection to the MetaTrader 5 terminal
 mt5.shutdown()
